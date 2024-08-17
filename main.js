d300efa69d45bf7b9f72fb9529e43b11e138363b
Julian <example@example.com>
2024-08-17 01:39:53 -0300

    Arreglo media queries de la pagina de pessi

---
+++ main.js
@@ -0,0 +1,30 @@
+function reproducirAudio(idAudio) {
+    var audio = document.getElementById(idAudio);
+    audio.currentTime = 0; // Reiniciar el audio si ya se está reproduciendo
+    audio.play();
+}
+
+function reiniciarAnimacion(idImagen, nuevaURL) {
+    let imagen = document.getElementById(idImagen);
+    imagen.src = nuevaURL;
+}
+
+const COLORS = ["#fff2", "#fff4", "#fff7", "#fffc"];
+
+const generateSpaceLayer = (size, selector, totalStars, duration) => {
+  const layer = [];
+  for (let i = 0; i < totalStars; i++) {
+    const color = COLORS[~~(Math.random() * COLORS.length)];
+    const x = Math.floor(Math.random() * 100);
+    const y = Math.floor(Math.random() * 100);
+    layer.push(`${x}vw ${y}vh 0 ${color}, ${x}vw ${y + 100}vh 0 ${color}`);
+  }
+  const container = document.querySelector(selector);
+  container.style.setProperty("--size", size);
+  container.style.setProperty("--duration", duration);
+  container.style.setProperty("--space-layer", layer.join(","));
+}
+
+generateSpaceLayer("2px", ".space-1", 250, "25s");
+generateSpaceLayer("3px", ".space-2", 100, "20s");
+generateSpaceLayer("6px", ".space-3", 25, "15s");
